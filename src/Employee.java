c72a412493f634472c54d749362a4f77c6b7db4b
Pavel <pavel.tsukanov@gmail.com>
2026-02-26 13:36:17 +0300

    Объявлена статическая переменная и инициализирована, используется для генерации уникальных id.

--- src/Employee.java
+++ src/Employee.java
@@ -1,11 +1,16 @@
 public class Employee {
+    // статическая переменная-счетчик
+    private static int counter = 0;
+
     // поля класса
+    private final int id; // ID номер сотрудника, final - не меняется
     private String fullName; // Ф.И.О.
     private int department; // отделы от 1 до 5
     private int salary; // зарплата в интервале 50–450
 
     // конструктор для создания обьекта
     public Employee(String fullName, int department, int salary) {
+        this.id = counter++;
         this.fullName = fullName;
         this.department = department;
         this.salary = salary;
@@ -14,7 +19,7 @@ public class Employee {
     // метод для вывода информации о сотруднике
     @Override
     public String toString() {
-        return "Сотрудник: " + fullName + ", Отдел: " + department + ", Зарплата: " + salary;
+        return "ID: " + id + "Сотрудник: " + fullName + ", Отдел: " + department + ", Зарплата: " + salary;
     }
 
 }
